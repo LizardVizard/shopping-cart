0c9f2c79e87baba2f18fe0af73c4b8d751dd40de
Maxim <195960242+LizardVizard@users.noreply.github.com>
2025-09-28 22:49:46 +0300

    test(CartItem): update tests to cover edge-cases for input validation

--- tests/CartItem.test.jsx
+++ tests/CartItem.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { describe, expect, it, beforeEach, vi } from "vitest";
 import { userEvent } from "@testing-library/user-event";
 
@@ -19,10 +19,9 @@ describe('Cart item component', () => {
         quantity: 3,
     }
 
-    const renderWithProps = (props
-    ) => {
+    const renderWithProps = (props) => {
         return render(<CartItem {...props}
-            itemQuantityChange={mockQuantityChange}
+            itemChangeQuantity={mockQuantityChange}
             itemDelete={mockDelete}
         />)
 
@@ -30,45 +29,153 @@ describe('Cart item component', () => {
 
     it('renders', () => {
         renderWithProps(initialData)
+        const input = screen.getByLabelText(/quantity/i)
         expect(screen.getByRole("heading")).toHaveTextContent(initialData.name)
-        expect(screen.getByRole("spinbutton")).toHaveValue(initialData.quantity)
-        expect(screen.getByRole("spinbutton")).toHaveValue(initialData.quantity)
+        expect(input).toHaveValue(initialData.quantity)
     })
 
 
     it('calls change callback on input change', async () => {
         const user = userEvent.setup()
         renderWithProps(initialData)
-        const input = screen.getByRole("spinbutton")
+        const input = screen.getByLabelText(/quantity/i)
 
-        await user.clear(input)
-        expect(mockQuantityChange).toHaveBeenCalledWith(0)
-        expect(mockQuantityChange).toHaveBeenCalledTimes(1)
+        // Initial data.quantity is 3
+        expect(input).toHaveValue(initialData.quantity)
 
         await user.type(input, "1")
-        expect(mockQuantityChange).toHaveBeenCalledWith(1)
-        expect(mockQuantityChange).toHaveBeenCalledTimes(2)
+        expect(mockQuantityChange).toHaveBeenCalledWith(31)
+        expect(mockQuantityChange).toHaveBeenCalledTimes(1)
+        expect(input).toHaveValue(31)
 
         await user.type(input, "3")
-        expect(mockQuantityChange).toHaveBeenCalledWith(13)
+        expect(mockQuantityChange).toHaveBeenCalledWith(313)
+        expect(mockQuantityChange).toHaveBeenCalledTimes(2)
+        expect(input).toHaveValue(313)
+
+        // await user.click(input)
+        await user.clear(input)
+        await user.type(input, "9")
+        expect(mockQuantityChange).toHaveBeenCalledWith(9)
         expect(mockQuantityChange).toHaveBeenCalledTimes(3)
+        expect(input).toHaveValue(9)
+
+
+        await user.type(input, "6")
+        expect(mockQuantityChange).toHaveBeenCalledWith(96)
+        expect(mockQuantityChange).toHaveBeenCalledTimes(4)
+        expect(input).toHaveValue(96)
+
+        await user.type(input, ".")
+
+        // NOTE: 16.3 turns into 6
+        await user.type(input, "3")
+        expect(mockQuantityChange).toHaveBeenCalledWith(96)
+        expect(mockQuantityChange).toHaveBeenCalledTimes(5)
+        expect(input).toHaveValue(96)
+
+
+        // Clears invalid input with default value of 1
+        await user.clear(input)
+        await user.tab()
+        expect(mockQuantityChange).toHaveBeenCalledWith(1)
+        expect(mockQuantityChange).toHaveBeenCalledTimes(6)
+        expect(input).toHaveValue(1)
+
     })
 
-    it('ignores non-numeric input', async () => {
-        const user = userEvent.setup()
-        renderWithProps(initialData)
-        const input = screen.getByRole("spinbutton")
+    // it('ignores invalid input', async () => {
+    //     const user = userEvent.setup()
+    //     renderWithProps(initialData)
+    //     // const input = screen.getByRole("spinbutton")
+    //     const input = screen.getByLabelText(/quantity/i)
+    //
+    //     expect(input).toHaveValue(initialData.quantity)
+    //
+    //     // console.log(input.value, "AWDAWIDH0")
+    //     // await user.clear(input)
+    //     // expect(mockQuantityChange).toHaveBeenCalledWith(1)
+    //     //
+    //     // await user.click(input)
+    //     console.log(input.value, "AWDAWIDH1")
+    //     await user.type(input, "abc")
+    //     // await user.type(input, "a")
+    //     // await user.type(input, "b")
+    //     // await user.type(input, "c")
+    //     // await user.tab()
+    //     console.log(input.value, "AWDAWIDH")
+    //     // expect(mockQuantityChange).not.toHaveBeenCalled()
+    //     expect(mockQuantityChange).toHaveBeenCalledWith(1)
+    //
+    //     await user.type(input, ".2")
+    //     await user.tab()
+    //     expect(mockQuantityChange).toHaveBeenCalledWith(1)
+    //     expect(input).toHaveValue(1)
+    //
+    //     console.log(input.value, "AWDAWIDH")
+    //     await user.clear(input)
+    //     await user.type(input, "2")
+    //     expect(mockQuantityChange).toHaveBeenCalledWith(2)
+    //
+    //     // expect(input).toHaveValue(1)
+    // })
 
-        expect(input).toHaveValue(initialData.quantity)
+    describe('changes from invalid input to a default value of 1', () => {
+        let input
+        beforeEach(() => {
+            renderWithProps(initialData)
+            input = screen.getByLabelText(/quantity/i)
+            expect(input).toHaveValue(initialData.quantity)
+        })
 
-        await user.type(input, "abc")
-        expect(mockQuantityChange).not.toHaveBeenCalled()
 
-        // await user.clear(input)
-        // await user.type(input, ".2")
-        // expect(mockQuantityChange).toHaveBeenCalledWith(0.2)
+        // NOTE:
+        // Chrome(Blink) blocks letters from number input, but Firefox(Gecko) does not.
+        // Because of that, onChange event is not called when typing non-numeric values
+        // when using Chrome based browsers.
+        // To keep the quantity input of type number, fireEvent is used to force value update.
+        test("for non-number values", () => {
+            fireEvent.change(input, { target: { value: "" } })
+            fireEvent.blur(input)
 
-        expect(input).toHaveValue(initialData.quantity)
+            expect(input).toHaveValue(1)
+            expect(mockQuantityChange).toHaveBeenCalledWith(1)
+
+            fireEvent.change(input, { target: { value: "a2" } })
+            fireEvent.blur(input)
+
+            expect(input).toHaveValue(1)
+            expect(mockQuantityChange).toHaveBeenCalledWith(1)
+        })
+        test("for out of range values", async () => {
+            // fireEvent.change(input, { target: { value: "0" } })
+            // fireEvent.blur(input)
+            //
+            // expect(input).toHaveValue(1)
+            // expect(mockQuantityChange).toHaveBeenCalledWith(1)
+            //
+            fireEvent.change(input, { target: { value: "-2" } })
+            fireEvent.blur(input)
+
+            expect(input).toHaveValue(1)
+            expect(mockQuantityChange).toHaveBeenCalledWith(1)
+
+            fireEvent.change(input, { target: { value: "3.2" } })
+            fireEvent.blur(input)
+
+            expect(input).toHaveValue(3)
+            expect(mockQuantityChange).toHaveBeenCalledWith(3)
+
+
+            // const user = userEvent.setup()
+            //
+            // await user.type(input, ".2")
+            // await user.tab()
+            //
+            // expect(mockQuantityChange).toHaveBeenCalledWith(3)
+            // expect(input).toHaveValue(3)
+        })
+
     })
 
     it('calls delete callback when delete button is pressed', async () => {
